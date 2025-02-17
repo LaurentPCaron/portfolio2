939475e0dd488350a9f913763261f14f73d8db2f
laurent <laurentpcaron@gmail.com>
2025-02-17 14:23:04 -0500

    Implement multilingual support: add language translations and update Header component for dynamic language switching

---
+++ src/i18n/ui.ts
@@ -0,0 +1,26 @@
+export const languages = {
+  en: "English",
+  fr: "Français",
+};
+
+export const defaultLang = "fr";
+
+export const ui = {
+  fr: {
+    "nav.home": "Accueil",
+    "nav.about": "À Propos",
+    "nav.projects": "Projets",
+    "nav.contact-me": "Me contacter",
+    "contact.title": "Contacts",
+    "contact.linkedIn": "LinkedIn",
+    "contact.github": "Github",
+    "contact.email": "Email",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.projects": "Projects",
+    "nav.contact-me": "Contact me",
+    "contact.email": "Couriel",
+  },
+} as const;
